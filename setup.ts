4a98b859f45ea841382f0ac8369c07e9066b17b9
Akhil2222 <akhily@Akhils-MacBook-Pro.local>
2024-03-26 11:18:50 -0500

    Quikfix

---
+++ setup.ts
@@ -0,0 +1,11 @@
+import {faker} from '@faker-js/faker'
+import {Database} from 'bun:sqlite'
+const Bun = require('bun')
+const db = new Database("SQLI.db");
+//@ts-ignore
+
+console.log(db.query(`select * from medicalRecords where userID='101' and passkey='14039520120718961347'`).all("baz"))
+
+// 101 - WeakPassword123
+
+db.close();
